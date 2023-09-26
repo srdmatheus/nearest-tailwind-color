fa508b701aeabdcd180b9979d66cbc8ee2057a79
Matheus Ribeiro <srdmatheus@gmail.com>
2023-09-26 11:03:44 -0300

    chore: add tailwind colors in object

---
+++ src/app/types/tailwind-colors.ts
@@ -0,0 +1,13 @@
+export type TailwindColor = {
+  r: number;
+  g: number;
+  b: number;
+};
+
+export type TailwindColors = {
+  [key: string]: {
+    r: number;
+    g: number;
+    b: number;
+  };
+};
