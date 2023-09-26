d0fffa635e962222b280232ae59285cf19a7dbfc
Matheus Ribeiro <srdmatheus@gmail.com>
2023-09-26 16:51:10 -0300

    chore: add color area exports

---
+++ src/components/color-area/index.tsx
@@ -0,0 +1,9 @@
+import { ColorSection } from "./color-section";
+import { ColorPicker } from "./color-picker";
+import { ColorButton } from "./color-button";
+
+export const ColorArea = {
+  Root: ColorSection,
+  Picker: ColorPicker,
+  Button: ColorButton,
+};
