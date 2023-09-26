6e33003d560b26e95951be4adccd49adb6bdcef3
Matheus Ribeiro <srdmatheus@gmail.com>
2023-09-26 16:50:35 -0300

    feat: add color picker component

---
+++ src/components/color-area/color-picker.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { ColorPicker as Picker, useColor } from "react-color-palette";
+import "react-color-palette/css";
+
+import { ColorButton } from "@/components/color-area/color-button";
+
+import { useColorContext } from "@/contexts/color";
+import { cn } from "@/utils/cn";
+
+export const ColorPicker = () => {
+  const [colorPicker, setColorPicker] = useColor("#38deb9");
+  const [showColorPicker, setShowColorPicker] = useState(true);
+
+  const { hex } = colorPicker;
+
+  const { dispatch } = useColorContext();
+  useEffect(() => {
+    dispatch({
+      type: "UPDATE_COLOR",
+      payload: hex,
+    });
+  }, [dispatch, hex]);
+
+  const handleOpenColorPicker = () => setShowColorPicker((prev) => !prev);
+
+  return (
+    <div className="relative">
+      <ColorButton onClick={handleOpenColorPicker}>{hex}</ColorButton>
+
+      <div
+        className={cn(
+          "-top-40 left-40",
+          showColorPicker ? "absolute" : "hidden",
+        )}
+      >
+        <Picker
+          hideAlpha
+          color={colorPicker}
+          onChange={setColorPicker}
+          height={150}
+          hideInput={["hsv"]}
+        />
+      </div>
+    </div>
+  );
+};
