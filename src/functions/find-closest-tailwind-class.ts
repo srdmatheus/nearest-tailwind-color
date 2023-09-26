f169c44123d2efebdaa1906464e0a93555fb6268
Matheus Ribeiro <srdmatheus@gmail.com>
2023-09-26 16:48:22 -0300

    refact: variable name changed

--- src/functions/find-closest-tailwind-class.ts
+++ src/functions/find-closest-tailwind-class.ts
@@ -5,18 +5,22 @@ import { TailwindColor } from "../types/tailwind-colors";
 export const findClosestTailwindClass = (userColor: TailwindColor) => {
   let closestClass = "";
   let closestDistance = Infinity;
-  let rgbColor = { r: 0, g: 0, b: 0 };
+  let rgbTailwindColor = { r: 0, g: 0, b: 0 };
 
   for (const className in tailwindColorClasses) {
     const tailwindColor = tailwindColorClasses[className];
     const distance = euclideanDistance(userColor, tailwindColor);
 
     if (distance < closestDistance) {
-      rgbColor = { r: tailwindColor.r, g: tailwindColor.g, b: tailwindColor.b };
+      rgbTailwindColor = {
+        r: tailwindColor.r,
+        g: tailwindColor.g,
+        b: tailwindColor.b,
+      };
       closestClass = className;
       closestDistance = distance;
     }
   }
 
-  return { closestClass, rgbColor };
+  return { closestClass, rgbTailwindColor };
 };
