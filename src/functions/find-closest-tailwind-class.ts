d192b99baa1feaf1aa5616c57d1636ab505bc231
Matheus Ribeiro <srdmatheus@gmail.com>
2023-09-26 18:04:02 -0300

    refact: parameter receives color in hex

--- src/functions/find-closest-tailwind-class.ts
+++ src/functions/find-closest-tailwind-class.ts
@@ -1,15 +1,22 @@
 import { tailwindColorClasses } from "@/constants/tailwind-colors";
 import { euclideanDistance } from "./euclidean-distance";
-import { TailwindColor } from "../types/tailwind-colors";
 
-export const findClosestTailwindClass = (userColor: TailwindColor) => {
+export const findClosestTailwindClass = (userColor: string) => {
   let closestClass = "";
   let closestDistance = Infinity;
   let rgbTailwindColor = { r: 0, g: 0, b: 0 };
 
+  const hex = userColor.replace(/^#/, "");
+
+  const rgbUserColor = {
+    r: parseInt(hex.slice(0, 2), 16),
+    g: parseInt(hex.slice(2, 4), 16),
+    b: parseInt(hex.slice(4, 6), 16),
+  };
+
   for (const className in tailwindColorClasses) {
     const tailwindColor = tailwindColorClasses[className];
-    const distance = euclideanDistance(userColor, tailwindColor);
+    const distance = euclideanDistance(rgbUserColor, tailwindColor);
 
     if (distance < closestDistance) {
       rgbTailwindColor = {
@@ -22,5 +29,5 @@ export const findClosestTailwindClass = (userColor: TailwindColor) => {
     }
   }
 
-  return { closestClass, rgbTailwindColor };
+  return { closestClass, rgbUserColor, rgbTailwindColor };
 };
